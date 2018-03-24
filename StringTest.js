2562a1be5f859913180704376a49d5cddbdc1adc
Jungzhang <zhanggen.jung@gmail.com>
2018-03-24 18:40:17 +0800

    init commit

---
+++ StringTest.js
@@ -0,0 +1,24 @@
+
+// 字符串
+var hello = "Hello World";
+console.log(hello);
+
+var name = '小明';
+var age = 20;
+
+// 模板ES6支持
+console.log(`你好, ${name}, 你今年${age}岁了!`);
+
+// 大小写转换
+console.log(hello.toLocaleUpperCase());
+console.log(hello.toLocaleLowerCase());
+
+// 子串在其中的位置
+console.log(hello.indexOf("ll"));
+console.log(hello.indexOf("aa"));
+
+// 取子串
+console.log(hello.substr(1,2));
+
+// HTML sub标签
+console.log(hello.sub());
